81cf097cb6bb12c689c9fa0cf1d3616488eae112
Yu Matsuzawa <yu.matsuzawa@access-company.com>
2017-10-13 19:00:03 +0900

    [master] Exclude priv/ from ctags

--- git/hooks/ctags.sh
+++ git/hooks/ctags.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Generic directories and acs related directories
-# Discard errors since warnings for nonexisting directoreis cannot be suppressed in Exuberant ctags
-ctags -R \
+# Discard errors since warnings for nonexisting directories cannot be suppressed in Exuberant ctags
+ctags --exclude=priv -R \
   app/ src/ lib/ web/ spec/ test/ script/ scripts/ \
   core/ eal/ testgear/lib/ testgear/web/ deps/solomon/ deps/dodai_client_elixir/ deps/croma/ \
   2> /dev/null && echo "Tags indexed."
