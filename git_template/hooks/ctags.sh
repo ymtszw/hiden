f7406646786865a7a14f48b995202d344e0f67a3
ymtszw <ymtszw@gmail.com>
2018-05-22 15:53:49 +0900

    [master] Update ctags target

--- git_template/hooks/ctags.sh
+++ git_template/hooks/ctags.sh
@@ -3,6 +3,6 @@
 # Discard errors since warnings for nonexisting directories cannot be suppressed in Exuberant ctags
 ctags --exclude=priv -R \
   app/ src/ lib/ web/ spec/ test/ script/ scripts/ \
-  core/ eal/ testgear/lib/ testgear/web/ deps/solomon/ deps/dodai_client_elixir/ deps/croma/ \
+  core/ eal/ local/ deps/antikythera/ deps/antikythera_acs/ deps/dodai_client_elixir/ deps/croma/ \
   2> /dev/null && echo "Tags indexed."
 
