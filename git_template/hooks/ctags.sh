fee7e02fe4317ebf618d2f51dd9d3405cf7d63ca
ymtszw <ymtszw@gmail.com>
2019-06-05 16:08:32 +0900

    [master] Update ctags opt

--- git_template/hooks/ctags.sh
+++ git_template/hooks/ctags.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# Generic directories and acs related directories
+# Generic directories and useful Elixir deps
 # Discard errors since warnings for nonexisting directories cannot be suppressed in Exuberant ctags
 ctags --exclude=priv -R \
-  app/ src/ lib/ web/ spec/ test/ script/ scripts/ \
-  core/ eal/ local/ deps/antikythera/ deps/antikythera_acs/ deps/dodai_client_elixir/ deps/croma/ \
+  app/ src/ lib/ web/ spec/ test/ script/ scripts/ core/ eal/ local/ \
+  deps/antikythera/ deps/croma/ deps/plug/ deps/phoenix/ \
   2> /dev/null && echo "Tags indexed."
 
