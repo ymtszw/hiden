b797e1f1dddf383149b0940cded5b41768b8f55a
Yu Matsuzawa <ymtszw@gmail.com>
2020-04-10 03:12:32 +0900

    [master] Add linuxbrew path

--- shrc.sh
+++ shrc.sh
@@ -7,6 +7,7 @@ export PATH="/usr/local/sbin/:$PATH"
 export PATH="/usr/local/bin/:$PATH"
 export PATH="$HOME/.mix/escripts:$PATH"
 export PATH="$HOME/bin:$PATH"
+export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 
 # Other exports
 
