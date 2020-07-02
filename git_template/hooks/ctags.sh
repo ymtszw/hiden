e13db4630492e15e9ad75671444d5460d80e78f0
Yu Matsuzawa <yu.matsuzawa@siiibo.com>
2020-07-02 21:05:39 +0900

    [master] Update shebang

--- git_template/hooks/ctags.sh
+++ git_template/hooks/ctags.sh
@@ -1,8 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # Generic directories and useful Elixir deps
 # Discard errors since warnings for nonexisting directories cannot be suppressed in Exuberant ctags
 ctags --exclude=priv -R \
   app/ src/ lib/ web/ spec/ test/ script/ scripts/ core/ eal/ local/ \
   deps/antikythera/ deps/croma/ deps/plug/ deps/phoenix/ \
   2> /dev/null
-
