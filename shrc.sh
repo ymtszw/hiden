a9ef3ac495d62b10875e006aaea80edf8389b335
Yu Matsuzawa <ymtszw@gmail.com>
2020-09-15 19:04:06 +0900

    chore: disable aws pager

--- shrc.sh
+++ shrc.sh
@@ -14,3 +14,4 @@ export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 export LESS='-g -i -M -R -S -W -z-4 -x2'
 export PAGER=less
 export ERL_AFLAGS="-kernel shell_history enabled"
+export AWS_PAGER=''
