6997b551716fc5ec509242e9fb9e5876f79b6a79
Yu Matsuzawa <yu.matsuzawa@siiibo.com>
2022-06-20 17:39:23 +0900

    doc: more description

--- install.sh
+++ install.sh
@@ -2,7 +2,31 @@
 set -euo pipefail
 
 #
-# devcontainer個人カスタマイズスクリプト
+# devcontainer個人カスタマイズ用スクリプト
+#
+#
+# Usage:
+#
+# * VSCodeのホスト側User設定で: Remote > Containers > Dotfilesに属する3項目を設定しておく
+# * devcontainerに対応している作業対象のrepositoryをcloneし、"Reopen in Container"する
+# * 詳しくは公式doc参照
+#   * https://code.visualstudio.com/docs/remote/containers#_personalizing-with-dotfile-repositories
+#
+#
+# Tips:
+#
+# * [追加VSCode拡張] devcontainer内部でこのスクリプトが実行されるのだが、VSCode側での実行順序の都合上、
+#   `code` CLIがこのスクリプトから利用できない模様（2022/06現在）
+#   * `code --install-extension`で好みのVSCode拡張をインストールしたかったが…
+#   * 結局、devcontainer立ち上げ完了後、ターミナルから手動で`cd ~/hiden; make vscode_extensions`している
+# * [Fishプラグイン] fishermanをインストールするところまではできるのだが、（`make ~/.config/fish/functions/fisher.fish`参照）
+#   なぜか`fish -c "fisher install ..."`と実際にfishermanを`fish -c`経由で呼び出しても成功しない
+#   * そこそこ掘ったのだが今のところ原因不明なので、こちらも手動で`cd ~/hiden; make fish_plugins`している
+# * [Gitで使うSSH秘密鍵] 予めホスト側のssh-agentで`ssh-add ~/.ssh/id_ecdsa`のように追加しておいてからdevcontainerを立ち上げれば、
+#   VSCodeがその秘密鍵をdevcontainerに持ち込んでくれるので、SSHプロトコルでそのままgitを利用できる
+#   * 一方、Windowsホストの場合など、ホスト側でssh-agentが使いづらいときはHTTPSプロトコルでgitを利用したほうが良い
+#   * git remoteとしてHTTPS URLが使用されている場合、VSCodeが管理する認証状態がdevcontainer内からのgit操作にも適用されるため
+#     * GitHubのrepositoryは少なくともこれに対応しているが、GitLab/BitBucketは不明（試してない and 調べてない）
 #
 
 echo "Customizing devcontainer..."
@@ -25,19 +49,25 @@ else
   fi
 fi
 
-# devcontainerはだいたいDebian/Ubuntuベースであると期待し、以下決め打ちでapt-getを使う
-$SUDO apt-get update
+# devcontainerはだいたいDebian/Ubuntuベースであると期待し、以下決め打ちでaptを使う
+# 複数のLinux distributionに対応したければ……頑張れ！
+$SUDO apt update
 
+# もともと個人的にMakefileで作成していた環境構築スクリプトを流用しているので、make必須
 if ! which make; then
-  $SUDO apt-get install -y make
+  $SUDO apt install -y make
 fi
 
+# 流石にcurlは最初から入っているdevcontainerが多いと思うが、念の為。自前ツールをインストールするときにもcurlを使うことは多い
 if ! which curl; then
-  $SUDO apt-get install -y curl
+  $SUDO apt install -y curl
 fi
 
 if ! which fish; then
-  $SUDO apt-get install -y fish
+  # 単にapt installすると古いバージョンのfishが降ってくることがある（というか大抵はそう）
+  # 厳密にやるならDebian/Ubuntuのバージョンごとに最新のリポジトリを引っ張ってきて登録し、apt updateしてからインストールする必要がある
+  # 判定ロジックが面倒なので、チームのdevcontainer自体に予め最新のfishをインストールしてもらうよう掛け合ったほうが早いかも
+  $SUDO apt install -y fish
 fi
 
 # fish config
@@ -45,7 +75,7 @@ if [ -f ~/.config/fish/config.fish ]; then
   rm ~/.config/fish/config.fish
 fi
 make ~/.config/fish/config.fish
-make ~/.config/fish/functions/fisher.fish # fisher installが何故かdotfilesスクリプトの中だと成功しないので、起動後に手動でやる
+make ~/.config/fish/functions/fisher.fish
 $SUDO chsh -s "$(which fish)" "$USER"
 
 # vim config
