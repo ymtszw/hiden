0269b1e7d155bbe03377e74986e2519509596cf0
Yu Matsuzawa <yu.matsuzawa@siiibo.com>
2022-07-06 21:39:55 +0900

    chore: bring back shrc.sh for ERL_AFLAGS

--- config.fish
+++ config.fish
@@ -50,3 +50,5 @@ function sync_history --on-event fish_preexec
     history --save
     history --merge
 end
+
+source shrc.sh
--- shrc.sh
+++ shrc.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 # Common settings for various shells
 
-# PATH
-
-export PATH="/usr/local/sbin:$PATH"
-export PATH="/usr/local/bin:$PATH"
-export PATH="$HOME/.mix/escripts:$PATH"
-export PATH="$HOME/bin:$PATH"
-export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
-
-# Other exports
-
 export LESS='-g -i -M -R -S -W -z-4 -x2'
 export PAGER=less
 export ERL_AFLAGS="-kernel shell_history enabled"
