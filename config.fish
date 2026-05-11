b314745d139b25e3c6a38f1f06a7cbe1e60f97d8
Yu Matsuzawa <ymtszw@gmail.com>
2026-05-11 18:55:08 +0900

    chore: disable GCloud prompt

--- config.fish
+++ config.fish
@@ -63,3 +63,4 @@ if which aws >/dev/null
     # Enable AWS CLI autocompletion: github.com/aws/aws-cli/issues/1079
     complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
 end
+
